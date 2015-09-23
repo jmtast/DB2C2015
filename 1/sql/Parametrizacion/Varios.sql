06102a94ffdfec1760a30b02d1265fff1b82d250
digarcia <diego.garcia.mail@gmail.com>
2015-09-23 01:02:51 -0300

    nuevo archivos de Parametrizacion

---
+++ 1/sql/Parametrizacion/Varios.sql
@@ -0,0 +1,43 @@
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Ruta Nacional');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Ruta Provincial');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Ruta Municipal');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Calle Asfaltada Urbana');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Calle Asfaltada Suburbana');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Avenida');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Autopista');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Calle de Tierra Urbana');
+INSERT INTO TipoDeCamino 				(Nombre) 					VALUES ('Calle de Tierra Suburbana');
+
+
+
+
+INSERT INTO ParametroBooleano 			(Nombre) 					VALUES ('Usaba Cinturon');
+INSERT INTO ParametroBooleano 			(Nombre) 					VALUES ('Funcionaron los Frenos');
+INSERT INTO ParametroBooleano 			(Nombre) 					VALUES ('Alcohol en Sangre');
+                                                                    
+                                                                    
+INSERT INTO ParametroString 			(Nombre) 					VALUES ('Estado de la Carretera');
+INSERT INTO ParametroString 			(Nombre) 					VALUES ('Nivel de Destruccion del Vehiculo');
+                                                                    
+INSERT INTO ParametroNumerico 			(Nombre) 					VALUES ('Velocidad de Circulacion');
+INSERT INTO ParametroNumerico 			(Nombre) 					VALUES ('Temperatura');
+INSERT INTO ParametroNumerico 			(Nombre) 					VALUES ('Porcentaje de Humedad');
+INSERT INTO ParametroNumerico 			(Nombre) 					VALUES ('Cantida de Alcohol en Sangre');
+
+INSERT INTO ModalidadDeAccidente 		(Nombre, Descripcion) 		VALUES ('Choque', 'Choque entre dos vehiculos');
+INSERT INTO ModalidadDeAccidente 		(Nombre, Descripcion) 		VALUES ('Choque Multiple', 'Choque entre mas dos vehiculos');
+INSERT INTO ModalidadDeAccidente 		(Nombre, Descripcion) 		VALUES ('Vuelco', 'Vuelco del vehiculo');
+INSERT INTO ModalidadDeAccidente 		(Nombre, Descripcion) 		VALUES ('Incendio', 'Incendio total o parcial del vehiculo');
+INSERT INTO ModalidadDeAccidente 		(Nombre, Descripcion) 		VALUES ('Choque a peaton', 'Choque a peaton individual o multiples peatones');
+INSERT INTO ModalidadDeAccidente 		(Nombre, Descripcion) 		VALUES ('Choque objeto inmovil', 'Choque a objeto inmovil. Incluye elementos de señalizacion.');
+
+
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Rosario Fernández', 'Policia Federal');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Rafael Amado', 'Policia Federal');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Emilio Bazán', 'Policia Federal');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Alfonso López', 'Gendarmeria');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Joaquín Martin', 'Gendarmeria');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Josefa Martínez', 'Policia Provincia Bs As');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('María Carrasco', 'Policia Provincia Bs As');
+INSERT INTO ResponsablePeritaje 		(Nombre, Organizacion) 		VALUES ('Daniel Machín', 'Fiscalia');
+	
---
+++ 1/sql/Parametrizacion/VehiculosYCategorias.sql
@@ -0,0 +1,45 @@
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (1, 'Motovehículos de 2 ruedas');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (2, 'Motovehículos de 3 o 4 ruedas');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (3, 'Automóviles particulares');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (4, 'Vehiculos de Carga');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (5, 'Transporte de pasajeros');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (6, 'Camiones y maquinaria especial no agrícola');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (7, 'Vehículos adaptados');
+INSERT INTO CategoríaDelVehículo		(idCategoria, Nombre) 		VALUES (8, 'Vehículos agrícolas');
+
+
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('FKL 075','Chevrolet', 'Onix',    '23-10-04',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('FKL 075','Chevrolet', 'Onix',    '23-10-04',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('MXS 020','Chevrolet', 'Celta',  '04-08-10',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('JPI 832','Chevrolet', 'Prisma',  '12-07-09',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('JSG 505','Chevrolet', 'Agile',   '05-02-05',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('QAR 196','Fiat', 'Punto',        '18-09-04',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('MFP 021','Fiat', 'Palio',        '11-02-10',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('JNA 945','Fiat', 'Strada',       '20-07-01',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('TOF 947','Fiat', '500',          '11-07-14',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('ESK 936','Ford', 'Focus',        '18-01-01',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('QYM 801','Ford', 'Fiesta',       '01-02-11',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('LBH 518','Ford', 'EcoSport',    '13-10-07',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('INU 827','Ford', 'Ranger',       '24-10-10',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('ZBU 632','Peugeot', '208',       '17-01-10',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('PDK 191','Peugeot', '206',       '19-02-08',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('RJC 584','Peugeot', '106',       '16-08-03',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('DSM 774','Peugeot', '207',       '20-12-04',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('EBP 018','Renault', 'Clio',      '04-04-03',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('OLW 167','Renault', 'Sandero',   '11-07-14',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('HCA 454','Renault', 'Kangoo',    '05-11-05',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('UVQ 241','Renault', 'Logan',     '22-04-11',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('BCQ 696','VW', 'Voyage',         '18-10-03',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('EYC 882','VW', 'Saveiro',        '30-07-01',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('MMY 861','VW', 'Fox',            '27-04-11',3);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('BAP 670','VW', 'Amarok',         '18-01-04',3);
+
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('WQL 324','MercedesBenz', '1633',     '16-08-11',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('BXB 105','MercedesBenz', '1618',     '11-04-09',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('MKJ 332','MercedesBenz', '1218',     '24-01-13',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('UIT 555','MercedesBenz', '1933',     '22-10-07',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('ROJ 403','Scania', 'Clase G',        '14-07-14',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('SUV 343','Scania', '113',            '08-02-09',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('QMX 832','Scania', 'P250',           '17-05-12',6);
+INSERT INTO Vehiculo 	(NroChapa, Marca, Modelo, FechaDePatentamiento, idCategoria) 	VALUES ('ODE 327','Scania', 'P310',           '11-04-06',6);
+ 
